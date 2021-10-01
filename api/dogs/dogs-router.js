4948c965d0503f9bf243e3462678e23fc68020af
Dave Kidd <dave@davekidd.net>
2021-09-30 19:04:55 -0600

    wip

--- api/dogs/dogs-router.js
+++ api/dogs/dogs-router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 // DOGS ENDPOINTS
 // DOGS ENDPOINTS
 // DOGS ENDPOINTS
-server.get('/api/dogs', (req, res) => {
+router.get('/api/dogs', (req, res) => {
   Dog.find()
     .then(dogs => {
       res.status(200).json(dogs);
